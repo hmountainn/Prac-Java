fd4e22b2e99e5e64b34657bb84abee32a4c7d318
hmountainn <ghkdwltks123@naver.com>
2021-03-14 18:40:01 +0900

    read함수 이용해서 입력받아 출력하기

--- src/Hello.java
+++ src/Hello.java
@@ -1,29 +1,27 @@
+import java.io.IOException;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		//콘솔 출력하기
-		//1.변수 설정
-		int kor, eng, math, total;
-		double avg;
+	public static void main(String[] args) throws IOException {
+		//read 58입력 -> 58그대로 출력하기
 		
-		kor = 60;
-		eng = 40;
-		math = 50;
+		//read는 하나만 읽어짐 (ab입력하면 a만)
+		//0입력하면 아스키코드값 48입력됨
+		System.out.print("국어성적을 입력하세요:");
+	
+		//58입력하면 5만 인식
+		int code1 = System.in.read();
+		System.out.printf("code is %d\n", code1); //아스키코드값  53
+		System.out.printf("code is %c\n", code1); //입력값 첫번째 
 		
-		total = kor + eng + math;
-		avg = total/3;
+		//8만 인식
+		int code2 = System.in.read();
+		System.out.printf("code is %d\n", code2); //아스키코드값 56
+		System.out.printf("code is %c\n", code2); //입력값 두번째
 		
-		//2. printf()로 형식지정해서 출력
-		System.out.println("┌─────────────────┐");
-		System.out.println("│     성적 출력      │");
-		System.out.println("└─────────────────┘");
-		System.out.printf("국어:%d\n", kor);
-		System.out.printf("영어:%d\n", eng);
-		System.out.printf("수학:%d\n", math);
-		System.out.printf("총점:%d\n", total);
-		System.out.printf("평균:%.1f\n ", avg);
-
+		int kor = (code1-48)*10+(code2-48); //입력한 값으로 조합
+		System.out.printf("국어성적 = %d", kor);
+		
 	}
 
 }
