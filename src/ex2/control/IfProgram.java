375c34458282f4823aa54257f3f6200f6c8d05bd
hmountainn <ghkdwltks123@naver.com>
2021-03-14 23:23:30 +0900

    <if-else문 사용>

--- src/ex2/control/IfProgram.java
+++ src/ex2/control/IfProgram.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 public class IfProgram {
 
 	public static void main(String[] args) {
-		//연속 if문 사용하기
+		//if-else문 사용하기
 		
 		//1.사용자로부터 정수 값 입력받아 변수x에 저장
 		Scanner scan = new Scanner(System.in);
 		System.out.print("정수 입력:");
 		int x = scan.nextInt();
 		
-		//2.3의배수와 5의배수 판단
-		//x가 3의배수, 5의배수 -> 범위겹치면 if 연속2개
-		if(x%3==0)
-			System.out.println("3의 배수");
-		if(x%5==0)
-			System.out.println("5의 배수");
+		//2. x가 3보다 작으면 "작은값" 5보다 크면 "큰값"
+		//   아무것도 속하지 않으면 "그냥 값" -> 범위 겹치지 않기때문에 if-else문 사용
+		if(x<3)
+			System.out.print("작은 값");
+		else if(x>5)
+			System.out.print("큰 값");
+		else
+			System.out.print("그냥 값");
 	}
 
 }
