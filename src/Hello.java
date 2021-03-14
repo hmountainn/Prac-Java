50203ffd5434dac5a80c65c49fe565fabb0bd6ab
hmountainn <ghkdwltks123@naver.com>
2021-03-14 19:42:48 +0900

    <FileInputStream으로 파일에서 입력받기>

--- src/Hello.java
+++ src/Hello.java
@@ -1,29 +1,25 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		//콘솔입력과 출력
+	public static void main(String[] args) throws IOException {
+		//FileInputStream이용해서 입력받기
 		
 		//1.변수 설정
 		int kor, eng, math, total;
 		double avg;
 		
-		
-		//2. Scanner이용해서 입력받기
-		Scanner scan = new Scanner(System.in);
-		System.out.println("┌─────────────────┐");
-		System.out.println("│     성적 입력      │");
-		System.out.println("└─────────────────┘");
-		
-		System.out.print("국어성적 입력:");
+		//2.FileInputStream으로 파일에서 입력받기
+		FileInputStream fis = new FileInputStream("C:\\Korweb\\2021-02\\workspace\\Prac JavaPrj\\data.txt");
+		Scanner scan = new Scanner(fis);
 		kor = scan.nextInt();
-		
-		System.out.print("영어성적 입력:");
 		eng = scan.nextInt();
-		
-		System.out.print("수학성적 입력:");
 		math = scan.nextInt();
+		
+		scan.close();
+		fis.close();
 		
 		total = kor + eng + math;
 		avg = total/3.0f;
