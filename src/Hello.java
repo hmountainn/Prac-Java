8b3323cca77551aa1114af17ad59729bccd17df5
hmountainn <ghkdwltks123@naver.com>
2021-03-14 19:51:27 +0900

    <FileInputStream과 FileOutputStream이용하기>

--- src/Hello.java
+++ src/Hello.java
@@ -1,11 +1,14 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Hello {
 
 	public static void main(String[] args) throws IOException {
 		//FileInputStream이용해서 입력받기
+		//FileOutputStream이용해서 파일에 출력하기
 		
 		//1.변수 설정
 		int kor, eng, math, total;
@@ -24,15 +27,15 @@ public class Hello {
 		total = kor + eng + math;
 		avg = total/3.0f;
 		
-		//3.출력
-		System.out.println("┌─────────────────┐");
-		System.out.println("│     성적 출력      │");
-		System.out.println("└─────────────────┘");
-		System.out.printf("국어:%d\n", kor);
-		System.out.printf("영어:%d\n", eng);
-		System.out.printf("수학:%d\n", math);
-		System.out.printf("총점:%d\n", total);
-		System.out.printf("평균:%.1f\n ", avg);
+		//3.FileOutputStream으로 파일에 출력
+		FileOutputStream fos = new FileOutputStream("C:\\Korweb\\2021-02\\workspace\\Prac JavaPrj\\data.txt");
+		PrintStream fout = new PrintStream(fos);
+		
+		fout.printf("총점:%d\n",total);
+		fout.printf("평균:%.1f", avg);
+		
+		fout.close();
+		fos.close();
 	}
 
 }
