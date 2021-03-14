638d7c45e58c20e1eabc63bd64ee67cbcb33d42d
hmountainn <ghkdwltks123@naver.com>
2021-03-14 23:19:24 +0900

    <if문 연속으로 사용하기>

--- src/ex2/control/IfProgram.java
+++ src/ex2/control/IfProgram.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class IfProgram {
 
 	public static void main(String[] args) {
-		//If문 사용하기
+		//연속 if문 사용하기
 		
 		//1.사용자로부터 정수 값 입력받아 변수x에 저장
 		Scanner scan = new Scanner(System.in);
 		System.out.print("정수 입력:");
 		int x = scan.nextInt();
 		
-		//2-1. 만약에 그 값이 짝수면 "짝수 값"이라는 문자열을 출력한다
-		if(x%2==0)
-			System.out.println("짝수");
-		else
-			System.out.println("홀수");
+		//2.3의배수와 5의배수 판단
+		//x가 3의배수, 5의배수 -> 범위겹치면 if 연속2개
+		if(x%3==0)
+			System.out.println("3의 배수");
+		if(x%5==0)
+			System.out.println("5의 배수");
 	}
 
 }
