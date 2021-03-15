b8342cc1df12c730b1352bf1d5971e49b7b29452
hmountainn <ghkdwltks123@naver.com>
2021-03-16 00:46:46 +0900

    <배열 생성 후 랜덤 값 넣기>

--- src/ex3/array/Program.java
+++ src/ex3/array/Program.java
@@ -1,18 +1,25 @@
 package ex3.array;
 
+import java.util.Random;
+
 public class Program {
 
 	public static void main(String[] args) {
-		//배열 생성 후 값 넣기
+		//배열 생성 후 랜덤 값 넣기
 		
-		//1.배열 생성
+		//1.Random객체 생성
+		Random rand = new Random();
+		
+		//2.배열 생성
 		int array[] = new int[5];
 		
-		//2.값 넣기
-		array[1] = 5;
+		//3.1~45범위 랜덤값 넣기
+		for(int i=0; i<array.length; i++)
+			array[i] = rand.nextInt(45)+1;
 		
 		//3.출력
-		System.out.println(array[1]);
+		for(int i=0; i<array.length; i++)
+			System.out.printf("%d ",array[i]);
 
 	}
 
