03e2730e7b090e9fad9217a31822ea4a19a12293
hmountainn <ghkdwltks123@naver.com>
2021-03-16 00:41:13 +0900

    <배열 생성 및 값 넣고 출력하기>

---
+++ src/ex2/array/Program.java
@@ -0,0 +1,19 @@
+package ex2.array;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//배열 생성 후 값 넣기
+		
+		//1.배열 생성
+		int array[] = new int[5];
+		
+		//2.값 넣기
+		array[1] = 5;
+		
+		//3.출력
+		System.out.println(array[1]);
+
+	}
+
+}
