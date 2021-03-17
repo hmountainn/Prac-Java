6c97665db276e335214a8728136c43338af0c8fe
hmountainn <ghkdwltks123@naver.com>
2021-03-17 23:57:12 +0900

    <배열에 숫자 넣고 섞어서 출력> + 섞은배열 순서대로 정렬해서 출력

--- src/ex3/array/test/Program2.java
+++ src/ex3/array/test/Program2.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Program2 {
 
 	public static void main(String[] args) {
-		//10개 크기의 배열에 1~10까지 숫자 넣고 섞어서 출력
+		//10개 크기의 배열에 1~10까지 숫자 넣고 섞어서 출력 + 섞은배열 순서대로 정렬해서 출력
 		
 		//1.10개 짜리 배열 nums생성
 		int[] nums = {1,2,3,4,5,6,7,8,9,10};
@@ -28,6 +28,23 @@ public class Program2 {
 		}
 			
 		//3.출력
+		for(int i=0; i<10; i++)
+			System.out.printf("%d ", nums[i]);
+		
+		System.out.println();
+		
+		//4.순서대로 정렬
+		int temp1;
+		for(int i=0; i<10; i++) 
+			for(int j=0; j<i; j++) {
+				if(nums[i]<nums[j]) {
+					temp1 = nums[i];
+					nums[i] = nums[j];
+					nums[j] = temp1;
+				}
+			}
+		
+		//5.출력
 		for(int i=0; i<10; i++)
 			System.out.printf("%d ", nums[i]);
 	}
