db5311d94a78de764f30aa3bff980963a0f08921
hmountainn <ghkdwltks123@naver.com>
2021-03-17 23:33:58 +0900

    <배열에 순환값 (1,2,3,1,2,3,1,2,3,1) 넣고 출력>

--- src/ex3/array/test/Program.java
+++ src/ex3/array/test/Program.java
@@ -3,17 +3,17 @@ package ex3.array.test;
 public class Program {
 
 	public static void main(String[] args) {
-		//배열에 짝수 넣고 출력하기
+		//배열에 순환값 (1,2,3,1,2,3,1,2,3,1) 넣고 출력
 		
 		//1.정수 10개짜리 nums 배열 생성
 		int[] nums = new int[10];
 		
-		//2.nums에 숫자를 짝수로 10개 채우기
+		//2.nums에 순환값 채우기
 		for(int i = 0; i<10; i++)
-			nums[i] = (i+1)*2;
+			nums[i] = i%3 + 1;
 		
 		//3.nums 출력
-		//출력모양 2,4,6, ... 20
+		//출력모양 1,2,3, ... 1
 		for(int i=0; i<10; i++)
 			if(i<9)
 				System.out.printf("%d,", nums[i]);
