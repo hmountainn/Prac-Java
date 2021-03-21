5085a04611b8eba1d8ec827d919b3db94d8858a3
hmountainn <ghkdwltks123@naver.com>
2021-03-21 18:49:40 +0900

    <문자열 조작하기>

--- src/ex3/array/test2/Program2.java
+++ src/ex3/array/test2/Program2.java
@@ -3,16 +3,13 @@ package ex3.array.test2;
 public class Program2 {
 
 	public static void main(String[] args) {
-		//문자열 비교하기( equals() vs == )
-		String n1 = "홍길동";
-		String n2 = "홍길동";
-		String n3 = new String("홍길동");
-		String n4 = new String("홍길동");
-		
-		System.out.println(n1 == n2); //true => 같은객체
-		System.out.println(n1.equals(n2)); //true
-		System.out.println(n3 == n4); //false => 객체를 비교하기 때문(서로다른객체)
-		System.out.println(n3.equals(n4)); //true => 문자열 자체를 비교
+		//문자열 조작
+		String fileName = "photo.jpg";
+		System.out.println(fileName.length());  //9
+		System.out.println(fileName.indexOf(".")); //5
+		System.out.println(fileName.substring(3)); //to.jpg
+		System.out.println(fileName.substring(3,5));// to
+		System.out.println(fileName.substring(0,fileName.indexOf(".")));// photo
 
 	}
 
