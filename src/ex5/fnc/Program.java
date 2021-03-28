9d8e599178162c797a97856372d64eb76e8cca3a
hmountainn <ghkdwltks123@naver.com>
2021-03-28 15:44:33 +0900

    <함수 정의 및 활용: 더하기 함수>

--- src/ex5/fnc/Program.java
+++ src/ex5/fnc/Program.java
@@ -5,12 +5,22 @@ public class Program { //함수 정의 및 활용
 	static int basic(int x){
 		return (x+3)*(x+3);
 	}
+	
+	//더하기 함수
+	static int add(int x,int y) {
+		return x+y;
+	}
 
+	
+	
 	public static void main(String[] args) {
 		//basic 활용
 		int result = basic(2);
-		System.out.printf("basic함수 리턴값:%d",result);
-
+		System.out.printf("basic함수 리턴값:%d\n",result);
+		
+		//add 활용
+		result = add(2,3);
+		System.out.printf("add함수 리턴값:%d\n",result);
 	}
 
 }
