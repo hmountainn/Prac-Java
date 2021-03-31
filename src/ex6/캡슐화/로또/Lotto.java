c890c97b7e48b5eb3f02ab7f9946551b1c852260
hmountainn <ghkdwltks123@naver.com>
2021-03-31 14:19:09 +0900

    <캡슐화 최종>

--- src/ex6/캡슐화/로또/Lotto.java
+++ src/ex6/캡슐화/로또/Lotto.java
@@ -1,6 +1,63 @@
 package ex6.캡슐화.로또;
 
+import java.util.Random;
+
 public class Lotto {
-	//Lotto객체안의 자료형 사용하기 위해 선언
-	int[] num;
+	//캡슐화3 (Lotto객체 쓰는함수들 가져오기)
+	
+	//변수 선언
+	private int[] num;
+	private int size;
+	
+	
+	//-------------------------------------
+	//로또 객체안의 변수 초기화하기
+	public static void init(Lotto x) {
+		x.size = 6;
+		x.num = new int[x.size];
+		
+	}
+	
+	//-------------------------------------
+	//private int size print()에서 사용할수있도록 하기
+	public static int getSize(Lotto lotto) {
+		return lotto.size;
+	}
+	
+	//-------------------------------------
+	//private int[] num print()에서사용할수있도록 하기
+	public static int getNum(Lotto lotto, int i) {
+		return lotto.num[i];
+	}
+	
+	
+	//-------------------------------------
+	// 로또번호 만들기
+	public static Lotto gen(Lotto lotto) {
+		Random rand = new Random();
+		
+		for(int i=0; i<6; i++) {
+			lotto.num[i]=rand.nextInt(45)+1;
+		}
+		
+		return lotto;	
+	}
+	
+	//-------------------------------------
+	// 로또번호 정렬
+	public static void sort(Lotto x) {
+		int temp;
+		for(int i=0; i<x.num.length; i++)
+			for(int j=0; j<i; j++) {
+				if(x.num[i]<x.num[j]) {
+					temp = x.num[j];
+					x.num[j] = x.num[i];
+					x.num[i] = temp;
+				}
+			}
+	}
+
+
+	
+	
 }
--- src/ex6/캡슐화/로또/Program.java
+++ src/ex6/캡슐화/로또/Program.java
@@ -7,55 +7,37 @@ public class Program {
 	public static void main(String[] args) {
 		
 		//로또 객체안의 자료형 사용하기
-		Lotto lotto;
+		Lotto lotto = new Lotto();
+		
+		//로또객체 초기화
+		Lotto.init(lotto);
 		
 		// 로또번호 만들기
-		lotto = gen();
+		lotto = Lotto.gen(lotto);
+		
 		// 로또번호 출력
 		print(lotto);
+		
 		// 로또번호 정렬
-		sort(lotto);
+		Lotto.sort(lotto);
+		
 		// 정렬된 번호 출력
 		print(lotto);
 
 	}
 	
-	//-------------------------------------
-	// 로또번호 만들기
-	private static Lotto gen() {
-		Random rand = new Random();
-		
-		Lotto list = new Lotto(); //로또객체 list라는 이름으로 사용할게
-		list.num = new int[6]; //로또객체안에 num배열 사용
-		
-		for(int i=0; i<6; i++) {
-			list.num[i]=rand.nextInt(45)+1;
-		}
-		
-		return list;	
-	}
+
 	
 	//-------------------------------------
 	// 로또번호 출력
+	//print는 콘솔 출력용이기 때문에 안옮김
 	private static void print(Lotto x) {
-		for(int i=0; i<x.num.length; i++) {
-			System.out.printf("["+"%d"+"] ",x.num[i]);
+		for(int i=0; i<Lotto.getSize(x); i++) {
+			System.out.printf("["+"%d"+"] ",Lotto.getNum(x, i));
 		}
 		System.out.println();
 	}
 	
-	//-------------------------------------
-	// 로또번호 정렬
-	private static void sort(Lotto x) {
-		int temp;
-		for(int i=0; i<x.num.length; i++)
-			for(int j=0; j<i; j++) {
-				if(x.num[i]<x.num[j]) {
-					temp = x.num[j];
-					x.num[j] = x.num[i];
-					x.num[i] = temp;
-				}
-			}
-	}
+
 
 }
