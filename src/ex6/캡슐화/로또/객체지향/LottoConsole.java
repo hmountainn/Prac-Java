b0417d2a384d0a24d59e4baa6e4de4984412ebce
hmountainn <ghkdwltks123@naver.com>
2021-03-31 21:39:19 +0900

    getSize호출 변경

--- src/ex6/캡슐화/로또/객체지향/LottoConsole.java
+++ src/ex6/캡슐화/로또/객체지향/LottoConsole.java
@@ -4,7 +4,7 @@ public class LottoConsole {
 	// 로또번호 출력
 	//static -> instance로 변경
 	public void print(Lotto x) {
-		for(int i=0; i<Lotto.getSize(x); i++) {
+		for(int i=0; i<x.getSize(x); i++) {
 			System.out.printf("["+"%d"+"] ",x.getNum(i));
 		}
 		System.out.println();
