212c3672a0768a799f30c5490efeaf34722bf3f4
hmountainn <ghkdwltks123@naver.com>
2021-04-07 20:08:56 +0900

    <추상화 재사용 준비>

---
+++ src/ex8/추상화/재사용/Exam.java
@@ -0,0 +1,28 @@
+package ex8.추상화.재사용;
+
+public abstract class Exam { //추상화 클래스
+	private int kor;
+	private int eng;
+	private int math;
+	
+	public Exam() {
+		this(10,10,10);
+	}
+	
+	public Exam(int kor, int eng, int math) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+	public int total() {
+		return kor+eng+math;
+	}
+	
+	public int total(int ext) { //오버로드 함수
+		return kor+eng+math;
+	}
+	
+	//추상화 메소드(자식들 무조건 구현해야함)
+	public abstract float avg();
+}
---
+++ src/ex8/추상화/재사용/ExamProgram.java
@@ -0,0 +1,20 @@
+package ex8.추상화.재사용;
+
+public class ExamProgram {
+
+	public static void main(String[] args) {
+		//YBMExam객체 사용
+		Exam exam = new YBMExam();
+		System.out.println(exam.avg());
+		
+		//배열통해 일괄사용
+		Exam[] exams = new Exam[2];
+		exams[0] = new NewExam();
+		exams[1] = new YBMExam();
+		
+		for(int i=0; i<2; i++) {
+			System.out.println(exams[i].avg());
+		}
+	}
+
+}
