c739ff7d7e4f19239f0e7da2f16d3959c294f52b
hmountainn <ghkdwltks123@naver.com>
2021-04-09 01:18:30 +0900

    add 주석

--- src/ex8/추상화/재사용/ExamProgram.java
+++ src/ex8/추상화/재사용/ExamProgram.java
@@ -4,7 +4,7 @@ public class ExamProgram {
 
 	public static void main(String[] args) {
 		
-		//NewExam사용
+		//NewExam사용(has a)
 		Exam exam = new NewExam();
 		
 		ExamConsole console = new ExamConsole();
@@ -13,7 +13,7 @@ public class ExamProgram {
 		console.print();
 		
 		
-		//YBMExam 사용
+		//YBMExam 사용(is a)
 		YBMExam exam1 = new YBMExam(1,1,1,1,1);
 		
 		YBMExamConsole ybmconsole = new YBMExamConsole();
