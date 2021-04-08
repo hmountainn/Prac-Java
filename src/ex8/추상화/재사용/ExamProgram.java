2b6613a63690a57faa25b8be210db01aba772bae
hmountainn <ghkdwltks123@naver.com>
2021-04-09 01:34:07 +0900

    <추상화 재사용: YBMExamConsole input()추가>

--- src/ex8/추상화/재사용/ExamProgram.java
+++ src/ex8/추상화/재사용/ExamProgram.java
@@ -4,24 +4,25 @@ public class ExamProgram {
 
 	public static void main(String[] args) {
 		
-		//NewExam사용(has a)
-		Exam exam = new NewExam();
-		
-		ExamConsole console = new ExamConsole();
-		console.setExam(exam);
-		
-		console.input();
-		console.print();
+//		//NewExam사용(has a)
+//		Exam exam = new NewExam();
+//		
+//		ExamConsole console = new ExamConsole();
+//		console.setExam(exam);
+//		
+//		console.input();
+//		console.print();
 
 		
 		
-//		//YBMExam 사용(is a)
-//		YBMExam exam1 = new YBMExam(1,1,1,1,1);
-//		
-//		YBMExamConsole ybmconsole = new YBMExamConsole();
-//		ybmconsole.setExam(exam1);
-//		
-//		ybmconsole.print();
+		//YBMExam 사용(is a)
+		YBMExam exam1 = new YBMExam(1,1,1,1,1);
+		
+		YBMExamConsole ybmconsole = new YBMExamConsole();
+		ybmconsole.setExam(exam1);
+		
+		ybmconsole.input();
+		ybmconsole.print();
 
 	}
 }
--- src/ex8/추상화/재사용/YBMExamConsole.java
+++ src/ex8/추상화/재사용/YBMExamConsole.java
@@ -1,5 +1,7 @@
 package ex8.추상화.재사용;
 
+import java.util.Scanner;
+
 public class YBMExamConsole extends ExamConsole {
 
 	@Override
@@ -7,5 +9,21 @@ public class YBMExamConsole extends ExamConsole {
 		YBMExam exam =(YBMExam)getExam(); //YBMExam객체 들고오기
 		System.out.printf("com:%d\n", exam.getCom());
 		System.out.printf("sci:%d\n", exam.getSci());
+	}
+	
+	@Override
+	public void input() {
+		super.input();
+		YBMExam exam = (YBMExam)getExam();
+		
+		int com,sci;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("com:");
+		com = scan.nextInt();
+		System.out.print("sci:");
+		sci = scan.nextInt();
+		
+		exam.setCom(com);
+		exam.setSci(sci);
 	}
 }
